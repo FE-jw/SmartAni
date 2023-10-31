540b2219306b841c13cbd3e832fb700eaafa2ce1
zaixu <zaixu91@gmail.com>
2023-10-31 16:42:10 +0900

    SmartAni 개발중

--- SmartAni.js
+++ SmartAni.js
@@ -3,29 +3,36 @@
 class SmartAni{
 	constructor(selector, options){
 		this.obj = document.querySelector(selector);
-		this.start = options.start;
-		this.end = options.end;
+		this.triggerElement = document.querySelector(options.triggerElement);
+		this.triggerHeight = options?.triggerHeight ?? 100;
+		this.start = null;
+		this.end = null;
 		this.css = options.css;
-		this.trigger = options?.trigger ?? false;
+		this.indicator = options?.indicator ?? false;
 		this.init();
-
-		['load', 'resize', 'orientationchange'].forEach(event => {
-			window.addEventListener(event, () => {
-				this.resetPosition(options);
-			});
-		});
+	}
+	
+	/**
+	 * trigger 활성화 지점 reset
+	 */
+	setTrigger(){
+		this.start = window.scrollY + this.triggerElement.getBoundingClientRect().top;
+		this.end = this.start + (this.triggerElement.offsetHeight * this.triggerHeight / 100);
 	}
 
-	triggerPoint(){
+	/**
+	 * trigger start, end 표시(테스트용)
+	 */
+	showIndicator(){
 		const triggerStart = document.createElement('div');
 		triggerStart.dataset.smartAniTrigger = '';
-		triggerStart.style.padding = '0.2em 0.5em';
+		triggerStart.style.padding = '0.2em 1.0em';
 		triggerStart.style.position = 'absolute';
-		triggerStart.style.right = 0;
+		triggerStart.style.left = 0;
 		triggerStart.style.fontSize = '16px';
 		triggerStart.style.color = '#fff';
 		triggerStart.style.backgroundColor = '#000';
-		triggerStart.style.zIndex = 100;
+		triggerStart.style.zIndex = 10000;
 		
 		const triggerEnd = triggerStart.cloneNode();
 		
@@ -40,17 +47,16 @@ class SmartAni{
 		document.body.append(triggerStart, triggerEnd);
 	}
 
-	resetPosition(options){
-		this.start = options.start;
-		this.end = options.end;
-	}
-
+	/**
+	 * 결과값 설정
+	 */
 	setTransform(){
 		const thisHeight = this.end - this.start;
 		const trigger = window.scrollY + window.innerHeight;
 		const triggerY = trigger - this.start;
 		const currentPercent = triggerY * 100 / thisHeight;
 		const effect = {};
+
 		effect.from = {
 			scaleX: this.css.scaleX?.from ?? 1,
 			scaleY: this.css.scaleY?.from ?? 1,
@@ -61,67 +67,93 @@ class SmartAni{
 		};
 		effect.to = {
 			scaleX: this.css.scaleX?.to ?? 1,
-			skewY: this.css.skewY?.to ?? 0,
-			skewX: this.css.skewX?.to ?? 0,
 			scaleY: this.css.scaleY?.to ?? 1,
+			skewX: this.css.skewX?.to ?? 0,
+			skewY: this.css.skewY?.to ?? 0,
 			translateX: this.css.translateX?.to ?? 0,
 			translateY: this.css.translateY?.to ?? 0
 		};
-		effect.value = {
-			scaleX: (effect.to.scaleX != effect.from.scaleX) ? (effect.to.scaleX - effect.from.scaleX) * currentPercent / 100 : 0,
-			scaleY: (effect.to.scaleY != effect.from.scaleY) ? (effect.to.scaleY - effect.from.scaleY) * currentPercent / 100 : 0,
-			skewX: (effect.to.skewX != effect.from.skewX) ? (effect.to.skewX - effect.from.skewX) * currentPercent / 100 : 0,
-			skewY: (effect.to.skewY != effect.from.skewY) ? (effect.to.skewY - effect.from.skewY) * currentPercent / 100 : 0,
-			translateX: (effect.to.translateX != effect.from.translateX) ? (effect.to.translateX - effect.from.translateX) * currentPercent / 100 : 0,
-			translateY: (effect.to.translateY != effect.from.translateY) ? (effect.to.translateY - effect.from.translateY) * currentPercent / 100 : 0
+		effect.change = {
+			scaleX: effect.to.scaleX - effect.from.scaleX,
+			scaleY: effect.to.scaleY - effect.from.scaleY,
+			skewX: effect.to.skewX - effect.from.skewX,
+			skewY: effect.to.skewY - effect.from.skewY,
+			translateX: effect.to.translateX - effect.from.translateX,
+			translateY: effect.to.translateY - effect.from.translateY
+		};
+		effect.result = {
+			scaleX: null,
+			scaleY: null,
+			skewX: null,
+			skewY: null,
+			translateX: null,
+			translateY: null
 		};
 
+		// opacity 있는 경우
+		if(this.css.opacity){
+			effect.from.opacity = this.css.opacity?.from ?? 1;
+			effect.to.opacity = this.css.opacity?.to ?? 1;
+			effect.change.opacity = effect.to.opacity - effect.from.opacity;
+			effect.result.opacity = null;
+		}
+		
+		// 결과값 저장
+		for(let prop in effect.result){
+			if(effect.to[prop] != effect.result[prop]){
+				effect.result[prop] = effect.from[prop] + (effect.change[prop] * currentPercent / 100);
+			}else{
+				effect.result[prop] = effect.from[prop] + effect.change[prop];
+			}
+		}
+
 		if(trigger >= this.end){
-			this.obj.style.setProperty('--smart-ani-scaleX', effect.to.scaleX);
-			this.obj.style.setProperty('--smart-ani-scaleY', effect.to.scaleY);
-			this.obj.style.setProperty('--smart-ani-skewX', effect.to.skewX);
-			this.obj.style.setProperty('--smart-ani-skewY', effect.to.skewY);
-			this.obj.style.setProperty('--smart-ani-translateX', effect.to.translateX);
-			this.obj.style.setProperty('--smart-ani-translateY', effect.to.translateY);
+			for(let key of Object.keys(effect.to)){
+				this.obj.style.setProperty(`--smart-ani-${key}`, effect.to[key]);
+			}
 		}else if(trigger <= this.start){
-			this.obj.style.setProperty('--smart-ani-scaleX', effect.from.scaleX);
-			this.obj.style.setProperty('--smart-ani-scaleY', effect.from.scaleY);
-			this.obj.style.setProperty('--smart-ani-skewX', effect.from.skewX);
-			this.obj.style.setProperty('--smart-ani-skewY', effect.from.skewY);
-			this.obj.style.setProperty('--smart-ani-translateX', effect.from.translateX);
-			this.obj.style.setProperty('--smart-ani-translateY', effect.from.translateY);
+			for(let key of Object.keys(effect.from)){
+				this.obj.style.setProperty(`--smart-ani-${key}`, effect.from[key]);
+			}
 		}else if(trigger > this.start && trigger < this.end){
-			this.obj.style.setProperty('--smart-ani-scaleX', effect.from.scaleX + effect.value.scaleX);
-			this.obj.style.setProperty('--smart-ani-scaleY', effect.from.scaleY + effect.value.scaleY);
-			this.obj.style.setProperty('--smart-ani-skewX', effect.from.skewX + effect.value.skewX);
-			this.obj.style.setProperty('--smart-ani-skewY', effect.from.skewY + effect.value.skewY);
-			this.obj.style.setProperty('--smart-ani-translateX', effect.from.translateX + effect.value.translateX);
-			this.obj.style.setProperty('--smart-ani-translateY', effect.from.translateY + effect.value.translateY);
+			for(let key of Object.keys(effect.result)){
+				this.obj.style.setProperty(`--smart-ani-${key}`, effect.result[key]);
+			}
 		}
 	}
 
-	init(options){
+	/**
+	 * 초기화 함수
+	 */
+	init(){
 		/**
-		 * transform:matrix(a, b, c, d, e, f);
+		 * transform:matrix(a, b, c, d, tx, ty);
 		 * a: scaleX
 		 * b: skewY
 		 * c: skewX
 		 * d: scaleY
-		 * e: translateX
-		 * f: translateY
+		 * tx: translateX
+		 * ty: translateY
 		 */
+
+		this.setTrigger();
 		this.setTransform();
 		this.obj.style.transform = 'matrix(var(--smart-ani-scaleX), var(--smart-ani-skewY), var(--smart-ani-skewX), var(--smart-ani-scaleY), var(--smart-ani-translateX), var(--smart-ani-translateY)';
+		if(this.css.opacity) this.obj.style.opacity = 'var(--smart-ani-opacity)';
 		
 		window.addEventListener('load', () => {
-			if(this.trigger){
-				this.triggerPoint();
-			}
+			if(this.indicator) this.showIndicator();
 		});
 
 		['load', 'scroll', 'resize', 'orientationchange'].forEach(event => {
 			window.addEventListener(event, () => {
 				this.setTransform();
+			});
+		});
+
+		['load', 'resize', 'orientationchange'].forEach(event => {
+			window.addEventListener(event, () => {
+				this.setTrigger();
 			});
 		});
 	}
